36ddad74f725061bc2d426535c33421585612a4e
rithul17 <rithul1905@gmail.com>
2024-03-16 22:49:43 +0530

    v2

--- client.py
+++ client.py
@@ -1,184 +1,76 @@
 import socket
-
 import ssl
-
 import os 
-
-
-
 HOST = '192.168.100.4'
-
 PORT = 12345
-
-
-
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 context = ssl.create_default_context()
-
 context.check_hostname = False
-
 context.verify_mode = ssl.CERT_NONE
-
 ssl_socket = context.wrap_socket(client_socket, server_hostname=HOST)
-
 ssl_socket.connect((HOST, PORT))
 
-
-
 username = input("Enter your username: ")
-
 pwd = input("Enter the password: ")
-
 ssl_socket.send(username.encode())
-
 ssl_socket.send(pwd.encode())
-
 client_ip=ssl_socket.getsockname()[0]
-
 auth = ssl_socket.recv().decode()
-
 if auth == "failed":
-
     print("Authentication failed")
-
     ssl_socket.close()
-
     exit()
-
 print("\nAuthenticated successfully")
-
-
-
 def read_data_from_file(filename):
-
     try:
-
         with open(filename, 'r') as file:
-
             data = file.read()
-
         return data
-
     except Exception as e:
-
         print("Error reading data from file:", e)
-
         return None
-
-
-
 while True:
-
     print("\n")
-
     print("What do you want to do:")
-
     print("1. Send data to server")
-
     print("2. Receive data from server")
-
     print("3. Delete file on server")
-
     print("4. View list of files on server")
-
     print("5. Exit")
-
     choice = input("Enter your choice as an integer: ")
-
-
-
     if choice == '1':
-
         ssl_socket.send("client_sending".encode())
-
-
-
         input_file = input("Enter the file name you want to store on the server: ")
-
         ssl_socket.send(input_file.encode())
-
         data_to_send = read_data_from_file(input_file)
-
-
-
         if data_to_send is not None:
-
             ssl_socket.sendall(data_to_send.encode())
-
             print("\nFile is sent to server")
-
             ack = ssl_socket.recv(1024).decode()
-
             print("Server acknowledgment:", ack)
-
-
-
     elif choice == '2':
-
         ssl_socket.send("client_receiving".encode())
-
-
-
         filename = input("Enter the filename to access: ")
-
         ssl_socket.send(filename.encode())
-
-
-
         data = ssl_socket.recv(4096).decode()
-
-
-
         client_folder = f"client_{client_ip}"
-
         if not os.path.exists(client_folder):
-
             os.makedirs(client_folder)
-
-
-
         with open(f"client_{client_ip}/{filename}", 'w') as file:
-
             file.write(data)
-
-
-
         print("\nData has been received from server and saved")
-
-
-
     elif choice == '3':
-
         ssl_socket.send("client_delete".encode())
-
         filename = input("Enter the filename to delete: ")
-
         ssl_socket.send(filename.encode())
-
         delete_response = ssl_socket.recv(1024).decode()
-
         print(delete_response)
-
-
-
     elif choice == '4':
-
         ssl_socket.send("client_list".encode())
-
         file_list = ssl_socket.recv(4096).decode()
-
         print("List of files on server:")
-
         print(file_list)
-
-
-
     elif choice == '5':
-
         ssl_socket.send("exit".encode())
-
         ssl_socket.close()
-
         break
-
--- server.py
+++ server.py
@@ -1,256 +1,110 @@
-
-
 import socket
-
 import ssl
-
 import os
-
 import threading
 
-
-
 HOST = '192.168.100.4'
-
 PORT = 12345
-
 CERTFILE = 'server.crt'
-
 KEYFILE = 'server.key'
 
-
-
 user_database = {
-
     "usr1": "pwd1",
-
     "usr2": "pwd2"
-
 }
 
-
-
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-
 context.load_cert_chain(certfile=CERTFILE, keyfile=KEYFILE)
-
 server_socket.bind((HOST, PORT))
-
 server_socket.listen()
-
 print('Server listening on {}:{}'.format(HOST, PORT))
 
-
-
 def authenticate_user(ssl_socket, addr):
-
     username = ssl_socket.recv(1024).decode()
-
     pwd = ssl_socket.recv(1024).decode()
-
-
-
     if username in user_database and user_database[username] == pwd:
-
         auth_username = username
-
         ssl_socket.send("success".encode())
-
         handle_client(ssl_socket, addr, auth_username)
-
     else:
-
         ssl_socket.send("failed".encode())
-
         print(f"{addr} has disconnected")
-
         ssl_socket.close()
-
         return False
-
-
-
 def get_next_index(client_folder, filename):
-
     file_name_without_extension, file_extension = os.path.splitext(filename)
-
-    
-
     if any(file.startswith(file_name_without_extension) for file in os.listdir(client_folder)):
-
         index = 1
-
         while True:
-
             new_filename = f"{file_name_without_extension}({index}){file_extension}"
-
             if not os.path.exists(f"{client_folder}/{new_filename}"):
-
                 return new_filename
-
             index += 1
-
     else:
-
         return f"{file_name_without_extension}{file_extension}"
-
-
-
 def handle_client(conn, addr, auth_username):
-
     while True:
-
         choice = conn.recv(1024).decode()
-
-
-
         if choice == 'client_sending':
-
             client_folder = f"svr_{auth_username}"
-
             if not os.path.exists(client_folder):
-
                 os.makedirs(client_folder)
-
-
-
             filename = conn.recv(1024).decode()
-
             data = conn.recv(4096).decode()
-
             print("Received data from client")
-
-
-
             filename = get_next_index(client_folder, filename)
-
-
-
             with open(f"{client_folder}/{filename}", 'w') as file:
-
                 file.write(data)
-
             print(f"Data has been written to {filename}")
-
-
-
             conn.send("Data has been written".encode())
-
             print("\n")
-
-
-
         elif choice == 'client_receiving':
-
             filename = conn.recv(1024).decode()
-
             with open(f"svr_{auth_username}/{filename}", 'r') as file:
-
                 data = file.read()
-
             conn.send(data.encode())
-
             print(f"Data has been sent to client with name {auth_username}")
-
             print("\n")
-
-
-
         elif choice == 'client_delete':
-
             filename = conn.recv(1024).decode()
-
             if delete_file(f"svr_{auth_username}", filename):
-
                 conn.send("File deleted successfully.".encode())
-
             else:
-
                 conn.send("Error deleting file.".encode())
-
-
-
         elif choice == 'client_list':
-
             files = list_files(f"svr_{auth_username}")
-
             if files:
-
                 files_str = '\n'.join(files)
-
                 conn.send(files_str.encode())
-
             else:
-
                 conn.send("No files found.".encode())
-
-
-
         elif choice == "exit":
-
             conn.close()
-
             break
-
-
-
 def list_files(client_folder):
-
     try:
-
         files = os.listdir(client_folder)
-
         return files
-
     except OSError as e:
-
         print(f"Error listing files: {e}")
-
         return None
-
-
-
 def delete_file(client_folder, filename):
-
     try:
-
         os.remove(os.path.join(client_folder, filename))
-
         print(f"File {filename} has been deleted.")
-
         return True
-
     except OSError as e:
-
         print(f"Error deleting file {filename}: {e}")
-
         return False
-
-
-
 try:
-
     while True:
-
         conn, addr = server_socket.accept()
-
         ssl_socket = context.wrap_socket(conn, server_side=True)
-
         print("Connected by", addr)
-
         print("\n")
-
         client_thread = threading.Thread(target=authenticate_user, args=(ssl_socket, addr))
-
         client_thread.start()
-
-
-
 except KeyboardInterrupt:
-
     print("\nServer interrupted. Closing server socket.")
-
     server_socket.close()
-
